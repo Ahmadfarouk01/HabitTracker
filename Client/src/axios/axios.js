09a0ef48e348b4de06d405f0812e7ad6fec68bdd
Ahmad Farouk <119784345+Ahmadfarouk01@users.noreply.github.com>
2026-02-03 18:29:59 +0100

    Update axios.js

--- Client/src/axios/axios.js
+++ Client/src/axios/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://habittracker-backend-232e.onrender.com/api",
 });
 
 export const setAuthToken = (token) => {
